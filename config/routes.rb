cc006f8b7704a19ed38d44b4044f5a113e11b75b
aniansson <ebba@aniansson.se>
2017-01-22 15:15:26 +0100

    Create routes and controller for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'landing/index'
+resources :articles do
+  resources :comments
+end
 
+  get 'landing/index'
   root controller: :landing, action: :index
 end
